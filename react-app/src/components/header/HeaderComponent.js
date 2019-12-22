80e26a61c24aaf6f1ace36b05779254602562dc1
kimseyoung <rlatpdud0309@gmail.com>
2019-12-22 13:31:58 +0900

    컴포넌트 만들자

---
+++ react-app/src/components/header/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <div className="useHeaderComponents">
+                <div className="useHeader__wrapper">
+                    <ul>
+                        <li>
+                            <a href="#none">LayLab</a>
+                            <ul>테스트</ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeaderComponent;
---
+++ react-app/src/routes/Routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+class Routes extends Component {
+    render() {
+        return (
+            <Router>
+                <Route path="/" component={} />
+            </Router>
+        );
+    }
+}
+
+export default Routes;
